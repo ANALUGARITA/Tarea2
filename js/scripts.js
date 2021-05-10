87eb5bda811b5ca8d2fee919c3cd119124e3c64d
¨ANALUGARITA¨ <analugarita@gmail.com>
2021-05-09 23:00:54 -0600

    Entrega Tarea

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,5 @@
 // Mapa Leaflet
-    var mapa = L.map('mapid').setView([9.8, -84.25], 7);
-
-// Definición de capas base
-    var capa_osm = L.tileLayer(
-      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?', 
-      {
-        maxZoom: 19,
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      }
-    ).addTo(mapa);	
+    var mapa = L.map('mapid').setView([9.65, -83.95], 10);
 
 // Conjunto de capas base
 	var osm = L.tileLayer(
@@ -38,7 +29,7 @@
         'https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}{r}.{ext}', 
         {
             attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-            subdomains: 'abcd', minZoom: 0, maxZoom: 18, ext: 'png'
+            subdomains: 'abcd', minZoom: 1, maxZoom: 17, ext: 'png'
         }
     ).addTo(mapa);         
         
@@ -54,35 +45,51 @@
     control_capas = L.control.layers(mapasBase).addTo(mapa);	
 // Control de escala
     L.control.scale({ position: 'topright', imperial: false }).addTo(mapa);       
-    
+
+
+
 
 // Capa vectorial en formato GeoJSON
-$.getJSON("https://tpb729-desarrollosigweb-2021.github.io/datos/sinac/areas_protegidas-wgs84.geojson", function(geodata) {
-  var capa_asp = L.geoJson(geodata, {
+$.getJSON("https://raw.githubusercontent.com/ANALUGARITA/Tarea2/main/capas/fincascafe.geojson", function(geodata) {
+  var capa_fincascafe = L.geoJson(geodata, {
     style: function(feature) {
-	  return {'color': "#013220", 'weight': 2.5, 'fillOpacity': 0.0}
-    },
+	  return {'color': "red", 'weight': 1.5, 'fillOpacity': 0.0}
+    },   
     onEachFeature: function(feature, layer) {
-      var popupText = "<strong>Área protegida</strong>: " + feature.properties.nombre_asp + "<br>" + "<strong>Categoría</strong>: " + feature.properties.cat_manejo;
-      layer.bindPopup(popupText);
+        var popupText = "<strong>Finca</strong>: " + feature.properties.Nombre + "<br>" + "<strong>Tipo de Café</strong>: " + feature.properties.TipoCafe;
+        layer.bindPopup(popupText);
     }			
   }).addTo(mapa);
 
-  control_capas.addOverlay(capa_asp, 'Áreas protegidas');
+  control_capas.addOverlay(capa_fincascafe, 'Fincas de Café');
  });
- 
-$.getJSON("https://tpb729-desarrollosigweb-2021.github.io/datos/ign/distritos-wgs84.geojson", function(geodata) {
-  var capa_dist = L.geoJson(geodata, {
+
+$.getJSON("https://raw.githubusercontent.com/ANALUGARITA/Tarea2/main/capas/viascafe.geojson", function(geodata) {
+  var capa_viascafe = L.geoJson(geodata, {
     style: function(feature) {
 	  return {'color': "red", 'weight': 1.5, 'fillOpacity': 0.0}
     },
     onEachFeature: function(feature, layer) {
-      var popupText = "<strong>Codigo Distrito</strong>: " + feature.properties.codigo_dta + "<br>" + "<strong>Nombre</strong>: " + feature.properties.distrito;
+      var popupText = "<strong>Vías a Fincas</strong>: " + feature.properties.RUTA;
       layer.bindPopup(popupText);
     }			
   }).addTo(mapa);
 
-  control_capas.addOverlay(capa_dist, 'Distritos'); 
+  control_capas.addOverlay(capa_viascafe, 'Vías a Fincas'); 
+});
+
+$.getJSON("https://raw.githubusercontent.com/ANALUGARITA/Tarea2/main/capas/cantonescafe.geojson", function(geodata) {
+  var capa_cantones = L.geoJson(geodata, {
+    style: function(feature) {
+	  return {'color': "black", 'weight': 1.5, 'fillOpacity': 0.0}
+    },
+    onEachFeature: function(feature, layer) {
+      var popupText = "<strong>Cantón</strong>: " + feature.properties.canton;
+      layer.bindPopup(popupText);
+    }			
+  }).addTo(mapa);
+
+  control_capas.addOverlay(capa_cantones, 'Cantones'); 
 });
 
 
